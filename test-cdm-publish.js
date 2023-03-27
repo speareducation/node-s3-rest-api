746cf4ee7167587c59e4aaf9a202aea23b0069ba
Jason Wright <jwright@speareducation.com>
2023-03-27 10:08:06 -0700

    DEVOPS-645 updated poc schema to match updates

--- test-cdm-publish.js
+++ test-cdm-publish.js
@@ -1,29 +1,39 @@
 require('dotenv').config();
 const sns = require('./sns');
+const { md5 } = require('./aws');
 
 const snsTopicArn = 'arn:aws:sns:us-east-1:009255884135:sns-cdm-dev.fifo';
 
 async function handle() {
 
-    const cdmPayload = {
-        timestamp: Date.now(),
-        transactionId: 'testtransactionasdf123',
-        operation: 'Update',
-        target: 'Person',
-        payload: {
-            spearUserId: 100532,
-            firstName: 'Jason',
-            lastName: 'Wright',
-            email: 'jwright@speareducation.com',
+    const source = 'netsuite';
+    const operation = 'update';
+    const target = 'person';
+    const person = {
+        id: 100532,
+        firstName: 'Jason',
+        lastName: 'Wright',
+        email: 'jwright@speareducation.com',
+    };
+
+    const groupId = source + '-' + operation;
+    const uniqueId = target + '-' + md5(JSON.stringify(person)).toString('hex');
+
+    const cdmEvent = {
+        eventHeader: {
+            timestamp: Date.now(),
+            transactionId: groupId + '-' + uniqueId,
+            operation: operation,
+            target: target,
+        },
+        eventPayload: {
+            person: person,
         },
     };
 
-    const uniqueId = cdmPayload.transactionId;
-    const groupId = cdmPayload.operation + '-' + cdmPayload.target;
-    
     // PUSH Change: send an update to SNS
     try {
-        const rsp = await sns.publish(snsTopicArn, groupId, uniqueId, JSON.stringify(cdmPayload));
+        const rsp = await sns.publish(snsTopicArn, groupId, uniqueId, JSON.stringify(cdmEvent));
         console.log(rsp);
     } catch (e) {
         console.error('Failed to publish event', e);
